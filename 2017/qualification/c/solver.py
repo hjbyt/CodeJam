dca0cabe680a1ca0fbf6235dc6c6ca1ed0e64e16
hjbyt <sopu00@gmail.com>
2017-04-08 14:11:11 +0300

    improved c

--- 2017/qualification/c/solver.py
+++ 2017/qualification/c/solver.py
@@ -17,7 +17,7 @@ def main():
     cases = parse_input()
 
     for i, N, K in cases:
-        r = solve(N, K)
+        r = solve2(N, K)
         r = ' '.join(str(x) for x in r)
         print(f'Case #{i}: {r}')
 
@@ -39,6 +39,33 @@ def solve(N, K):
         heappush(chunks, -a)
         heappush(chunks, -b)
     return sorted([a, b], reverse=True)
+
+
+def solve2(N, K):
+    if K == N:
+        return 0, 0
+    c = N - 1
+    a = c // 2
+    b = c - a
+    if K == 1:
+        return b, a
+
+    if K == 2:
+        return solve2(b, 1)
+
+    if N % 2 == 1:
+        if K % 2 == 1:
+            return solve2((N - 1) // 2, (K - 1) // 2)
+        else:
+            return solve2((N - 1) // 2, (K - 1) // 2 + 1)
+
+    k = K - 1
+    ka = k // 2
+    kb = k - ka
+    sa = solve2(a, ka)
+    sb = solve2(b, kb)
+    return min([sa, sb], key=lambda x: (x[1], x[0]))
+
 
 if __name__ == '__main__':
     main()
