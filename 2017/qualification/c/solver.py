a716b29c608762040161471ee6a96f58c3bf2738
hjbyt <sopu00@gmail.com>
2017-04-08 14:39:13 +0300

    improved c

--- 2017/qualification/c/solver.py
+++ 2017/qualification/c/solver.py
@@ -2,22 +2,28 @@ from heapq import heappush, heappop
 from io import StringIO
 import sys
 
+# TEST = '''\
+# 5
+# 4 2
+# 5 2
+# 6 2
+# 1000 1000
+# 1000 1
+# '''
 TEST = '''\
-5
-4 2
-5 2
-6 2
-1000 1000
-1000 1'''
+1
+999999999999999995 30303030303030303
+'''
 
-# sys.stdin = StringIO(TEST)
+
+sys.stdin = StringIO(TEST)
 
 
 def main():
     cases = parse_input()
 
     for i, N, K in cases:
-        r = solve2(N, K)
+        r = solve(N, K)
         r = ' '.join(str(x) for x in r)
         print(f'Case #{i}: {r}')
 
@@ -30,41 +36,32 @@ def parse_input():
 
 
 def solve(N, K):
-    chunks = [-N]
-    for _ in range(K):
-        c = -heappop(chunks)
-        c -= 1
-        a = c // 2
-        b = c - a
-        heappush(chunks, -a)
-        heappush(chunks, -b)
-    return sorted([a, b], reverse=True)
-
-
-def solve2(N, K):
     if K == N:
         return 0, 0
-    c = N - 1
-    a = c // 2
-    b = c - a
-    if K == 1:
-        return b, a
 
-    if K == 2:
-        return solve2(b, 1)
-
-    if N % 2 == 1:
-        if K % 2 == 1:
-            return solve2((N - 1) // 2, (K - 1) // 2)
-        else:
-            return solve2((N - 1) // 2, (K - 1) // 2 + 1)
+    n = N - 1
+    a = n // 2
+    b = n - a
 
     k = K - 1
     ka = k // 2
     kb = k - ka
-    sa = solve2(a, ka)
-    sb = solve2(b, kb)
-    return min([sa, sb], key=lambda x: (x[1], x[0]))
+
+    if K == 1:
+        return b, a
+
+    if K == 2:
+        return solve(b, 1)
+
+    if n % 2 == 0:
+        return solve(b, kb)
+    else:
+        if k % 2 == 0:
+            return solve(a, ka)
+
+        sa = solve(a, ka)
+        sb = solve(b, kb)
+        return min([sa, sb], key=lambda x: (x[1], x[0]))
 
 
 if __name__ == '__main__':
