2181f53e050cbfa64a6742ddae72a4d077151886
hjbyt <sopu00@gmail.com>
2017-04-08 15:21:56 +0300

    improved a (hopefully correct)

--- 2017/qualification/a/solver.py
+++ 2017/qualification/a/solver.py
@@ -30,41 +30,26 @@ def parse_input():
 
 
 def solve(S, K):
-    g = make_graph(S, K)
-    dest = '+' * len(S)
-    if not g.has_node(dest):
-        return 'IMPOSSIBLE'
+    last_index = len(S) - K
+    c = 0
+    for i in range(last_index):
+        if S[i] != +1:
+            flip(S, K, i)
+            c += 1
+    last = S[last_index:]
+    if len(last) != K:
+        raise Exception('unexpected')
+    if all(x == +1 for x in last):
+        return c
+    elif all(x == -1 for x in last):
+        return c + 1
     else:
-        return nx.shortest_path_length(g, to_string(S), dest)
-
-
-def make_graph(S, K):
-    g = nx.DiGraph()
-    S_string = to_string(S)
-    g.add_node(S_string)
-    add_neighbours(S, S_string, K, g)
-    return g
-
-
-def add_neighbours(S, S_string, K, g):
-    for i in range(0, len(S) - K + 1):
-        flipped = flip(S, K, i)
-        string = to_string(flipped)
-        had_node = g.has_node(string)
-        g.add_edge(S_string, string)
-        if not had_node:
-            add_neighbours(flipped, string, K, g)
+        return 'IMPOSSIBLE'
 
 
 def flip(S, K, i):
-    S = S[:]
     for j in range(i, i + K):
         S[j] *= -1
-    return S
-
-
-def to_string(S):
-    return ''.join('-' if x == -1  else '+' for x in S)
 
 
 if __name__ == '__main__':
